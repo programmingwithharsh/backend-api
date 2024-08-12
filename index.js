d2394b507c78ef10c6c46d648010d1590236777f
Harsh Tiwari <harshtiwari343@email.com>
2024-08-12 11:13:04 +0530

    Added CORS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const db = require("./queries");
 const bodyParser = require("body-parser");
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
